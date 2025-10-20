cfa1694137910eff07908a2ff7ef2086bae82b92
ebner <simon.ebner@psi.ch>
2025-10-20 10:36:19 +0200

    reformatting code with black

--- cbf.py
+++ cbf.py
@@ -1,4 +1,3 @@
-
 import numpy
 import hashlib
 import base64
@@ -7,40 +6,41 @@ import cbf_c
 import re
 
 miniheader_re = re.compile(
-    b'(.*^data_(?P<prefix>.*?)_\d+\r\n)?'
-    b'(.*^# Detector: (?P<detector_model>[^,]+),\s*(?P<detector_number>[^,\r\n]+))?'
-    b'(.*^# Pixel_size (?P<x_pixel_size>[^ m]+) m x (?P<y_pixel_size>[^ m]+))?'
-    b'(.*^# Silicon sensor, thickness (?P<sensor_thickness>[^ ]+))?'
-    b'(.*^# Exposure_time (?P<exposure_time>.+?) s\r\n)?'
-    b'(.*^# Exposure_period (?P<exposure_period>.+?) s\r\n)?'
-    b'(.*^# Threshold_setting: (?P<threshold_energy>[^ ]+ eV))?'
-    b'(.*^# Gain_setting: (?P<threshold_gain>[^\r\n]+))?'
-    b'(.*^# Image_path: (?P<image_path>[^\r\n]+))?'
-    b'(.*^# Wavelength (?P<wavelength>.*?) A)?'
-    b'(.*^# Detector_distance (?P<detector_distance>.*?) m)?'
-    b'(.*^# Beam_xy \((?P<beam_center_x>\d+\.\d+), (?P<beam_center_y>\d+\.\d+)\) pixels)?'
-    b'(.*^# Flux (?P<photon_flux>[^\r\n]+))?'
-    b'(.*^# Filter_transmission (?P<beam_attenuation>[^\r\n]+))?'
-    b'(.*^# Start_angle (?P<start_angle>.*?) deg.)?'
-    b'(.*^# Angle_increment (?P<angle_increment>.*?) deg.)?'
-    b'(.*^# Phi (?P<phi>.*?) deg.)?'
-    b'(.*^# Phi_increment (?P<phi_increment>.*?) deg.)?'
-    b'(.*^# Chi (?P<chi>.*?) deg.)?'
-    b'(.*^# Chi_increment (?P<chi_increment>.*?) deg.)?'
-    b'(.*^# Oscillation_axis (?P<oscillation_axis>[^\r\n]+))?'
+    b"(.*^data_(?P<prefix>.*?)_\d+\r\n)?"
+    b"(.*^# Detector: (?P<detector_model>[^,]+),\s*(?P<detector_number>[^,\r\n]+))?"
+    b"(.*^# Pixel_size (?P<x_pixel_size>[^ m]+) m x (?P<y_pixel_size>[^ m]+))?"
+    b"(.*^# Silicon sensor, thickness (?P<sensor_thickness>[^ ]+))?"
+    b"(.*^# Exposure_time (?P<exposure_time>.+?) s\r\n)?"
+    b"(.*^# Exposure_period (?P<exposure_period>.+?) s\r\n)?"
+    b"(.*^# Threshold_setting: (?P<threshold_energy>[^ ]+ eV))?"
+    b"(.*^# Gain_setting: (?P<threshold_gain>[^\r\n]+))?"
+    b"(.*^# Image_path: (?P<image_path>[^\r\n]+))?"
+    b"(.*^# Wavelength (?P<wavelength>.*?) A)?"
+    b"(.*^# Detector_distance (?P<detector_distance>.*?) m)?"
+    b"(.*^# Beam_xy \((?P<beam_center_x>\d+\.\d+), (?P<beam_center_y>\d+\.\d+)\) pixels)?"
+    b"(.*^# Flux (?P<photon_flux>[^\r\n]+))?"
+    b"(.*^# Filter_transmission (?P<beam_attenuation>[^\r\n]+))?"
+    b"(.*^# Start_angle (?P<start_angle>.*?) deg.)?"
+    b"(.*^# Angle_increment (?P<angle_increment>.*?) deg.)?"
+    b"(.*^# Phi (?P<phi>.*?) deg.)?"
+    b"(.*^# Phi_increment (?P<phi_increment>.*?) deg.)?"
+    b"(.*^# Chi (?P<chi>.*?) deg.)?"
+    b"(.*^# Chi_increment (?P<chi_increment>.*?) deg.)?"
+    b"(.*^# Oscillation_axis (?P<oscillation_axis>[^\r\n]+))?"
     b'(.*conversions="(?P<conversions>[^"]+)")?'
-    b'(.*X-Binary-Size: (?P<binary_size>[^\r\n]+))?'
-    b'(.*X-Binary-ID: (?P<binary_id>[^\r\n]+))?'
+    b"(.*X-Binary-Size: (?P<binary_size>[^\r\n]+))?"
+    b"(.*X-Binary-ID: (?P<binary_id>[^\r\n]+))?"
     b'(.*X-Binary-Element-Type: "(?P<binary_element_type>[^"]+)")?'
-    b'(.*X-Binary-Element-Byte-Order: (?P<binary_element_byte_order>[^\r\n]+))?'
-    b'(.*Content-MD5:\s*(?P<content_md5>[^\r\n]+))?'
-    b'(.*X-Binary-Number-of-Elements: (?P<number_of_elements>[^\r\n]+))?'
-    b'(.*X-Binary-Size-Fastest-Dimension: (?P<pixels_in_x>[^\r\n]+))?'
-    b'(.*X-Binary-Size-Second-Dimension: (?P<pixels_in_y>[^\r\n]+))?'
-    b'(.*X-Binary-Size-Padding: (?P<binary_padding>[^\r\n]+))?',
-    flags=re.MULTILINE | re.DOTALL)
+    b"(.*X-Binary-Element-Byte-Order: (?P<binary_element_byte_order>[^\r\n]+))?"
+    b"(.*Content-MD5:\s*(?P<content_md5>[^\r\n]+))?"
+    b"(.*X-Binary-Number-of-Elements: (?P<number_of_elements>[^\r\n]+))?"
+    b"(.*X-Binary-Size-Fastest-Dimension: (?P<pixels_in_x>[^\r\n]+))?"
+    b"(.*X-Binary-Size-Second-Dimension: (?P<pixels_in_y>[^\r\n]+))?"
+    b"(.*X-Binary-Size-Padding: (?P<binary_padding>[^\r\n]+))?",
+    flags=re.MULTILINE | re.DOTALL,
+)
 
-header_base = '''_array_data.data\r
+header_base = """_array_data.data\r
 ;\r
 --CIF-BINARY-FORMAT-SECTION--\r
 Content-Type: application/octet-stream;\r
@@ -55,9 +55,9 @@ X-Binary-Number-of-Elements: {number_of_elements:d}\r
 X-Binary-Size-Fastest-Dimension: {size_fastest_dimension:d}\r
 X-Binary-Size-Second-Dimension: {size_second_dimension:d}\r
 X-Binary-Size-Padding: {size_padding:d}\r
-\r\n'''
+\r\n"""
 
-header_end_mark = b'\x0C\x1A\x04\xD5'
+header_end_mark = b"\x0c\x1a\x04\xd5"
 
 end_binary_section = "\r\n--CIF-BINARY-FORMAT-SECTION----\r\n;\r\n"
 
@@ -74,13 +74,13 @@ def write(filename, data, header=None, size_padding=0):
 
     # Check input data
     if data.itemsize == 4:
-        element_type = 'signed 32-bit integer'
+        element_type = "signed 32-bit integer"
     # elif data.itemsize == 2:
     #     element_type = 'signed 16-bit integer'
     else:
         raise TypeError(str(data.dtype))
 
-    compression_algorithm = 'x-CBF_BYTE_OFFSET'
+    compression_algorithm = "x-CBF_BYTE_OFFSET"
 
     # Compress data
     try:
@@ -88,29 +88,34 @@ def write(filename, data, header=None, size_padding=0):
         output_buffer_size = output_buffer.size
     except:
         # The cbf compression was not able to compress the data
-        compression_algorithm = 'x-CBF_NONE'
+        compression_algorithm = "x-CBF_NONE"
         output_buffer = data.tobytes()
         output_buffer_size = data.nbytes
 
     md5_hash = base64.b64encode(hashlib.md5(output_buffer).digest()).decode()
 
     # Write file
-    file_handle = open(filename, 'wb')
+    file_handle = open(filename, "wb")
     # file_handle.write(header['version'])
     # file_handle.write(header['convention'])
     # file_handle.write(header['contents'])
-    file_handle.write(header_base.format(compression_algorithm=compression_algorithm,
-                                         binary_size=output_buffer_size,
-                                         number_of_elements=data.size,
-                                         size_fastest_dimension=data.shape[1],
-                                         size_second_dimension=data.shape[0],
-                                         md5_hash=md5_hash, element_type=element_type,
-                                         size_padding=size_padding).encode())
+    file_handle.write(
+        header_base.format(
+            compression_algorithm=compression_algorithm,
+            binary_size=output_buffer_size,
+            number_of_elements=data.size,
+            size_fastest_dimension=data.shape[1],
+            size_second_dimension=data.shape[0],
+            md5_hash=md5_hash,
+            element_type=element_type,
+            size_padding=size_padding,
+        ).encode()
+    )
     file_handle.write(header_end_mark)
     file_handle.write(output_buffer)
 
     if size_padding:
-        padding_bytes = b'\x00'*size_padding
+        padding_bytes = b"\x00" * size_padding
         file_handle.write(padding_bytes)
     file_handle.write(end_binary_section.encode())
 
@@ -126,7 +131,7 @@ def read(filename, metadata=True, parse_miniheader=False):
     :return: Data (namedtuple('data', 'metadata', 'miniheader')), None for both metadata/miniheader if False in params.
     """
 
-    file_descriptor = open(filename, 'rb')
+    file_descriptor = open(filename, "rb")
     file_content = file_descriptor.read()
     file_descriptor.close()
 
@@ -135,15 +140,15 @@ def read(filename, metadata=True, parse_miniheader=False):
 
     # Read header
     file_header = file_content[:header_end_index]
-    pattern = re.compile(r'X-Binary-([\w-]*)([\s:]*)(.*)')
-    pattern_md5 = re.compile(r'Content-MD5:\s*(.*)')
+    pattern = re.compile(r"X-Binary-([\w-]*)([\s:]*)(.*)")
+    pattern_md5 = re.compile(r"Content-MD5:\s*(.*)")
     pattern_compression = re.compile(r'\s*conversions="(.*)"')
     header = dict()
 
     for line in file_header.decode().splitlines():
         m = pattern.search(line)
         if m:
-            key = m.group(1).lower().replace('-', '_')  # Sanitize key names
+            key = m.group(1).lower().replace("-", "_")  # Sanitize key names
             val = m.group(3).strip()
             try:
                 val = int(val)
@@ -156,11 +161,11 @@ def read(filename, metadata=True, parse_miniheader=False):
         else:
             m = pattern_md5.search(line)
             if m:
-                header['md5'] = m.group(1).strip()
+                header["md5"] = m.group(1).strip()
             else:
                 m = pattern_compression.search(line)
                 if m:
-                    header['compression'] = m.group(1).strip()
+                    header["compression"] = m.group(1).strip()
 
     # print(header)
 
@@ -182,34 +187,43 @@ def read(filename, metadata=True, parse_miniheader=False):
                 try:
                     miniheader[k] = float(v)
                 except (ValueError, TypeError) as e:
-                    miniheader[k] = v.decode('utf-8')
+                    miniheader[k] = v.decode("utf-8")
                 except:
                     pass
 
     # Read binary data
-    input_buffer = file_content[header_end_index + len(header_end_mark):][:header['size']]
+    input_buffer = file_content[header_end_index + len(header_end_mark) :][
+        : header["size"]
+    ]
     # print(base64.b64encode(hashlib.md5(input_buffer).digest()))
 
     # Uncompress data
-    if header['element_type'] == u'"signed 32-bit integer"': # DECTRIS PILATUS format
+    if header["element_type"] == '"signed 32-bit integer"':  # DECTRIS PILATUS format
         data_type = numpy.int32
-    elif '32' in header['element_type']:
+    elif "32" in header["element_type"]:
         data_type = numpy.uint32
     # elif '16' in header['element_type']:
     #     data_type = numpy.uint16
     else:
-        raise TypeError('Type not supported'+header['element_type'])
+        raise TypeError("Type not supported" + header["element_type"])
 
-    if header['compression'] == "x-CBF_BYTE_OFFSET":
-        data = uncompress(input_buffer, header['size_second_dimension'], header['size_fastest_dimension'], data_type)
-    elif header['compression'] == "x-CBF_NONE":
+    if header["compression"] == "x-CBF_BYTE_OFFSET":
+        data = uncompress(
+            input_buffer,
+            header["size_second_dimension"],
+            header["size_fastest_dimension"],
+            data_type,
+        )
+    elif header["compression"] == "x-CBF_NONE":
         data = numpy.frombuffer(input_buffer, dtype=data_type)
-        data = data.reshape((header['size_second_dimension'], header['size_fastest_dimension']))
+        data = data.reshape(
+            (header["size_second_dimension"], header["size_fastest_dimension"])
+        )
     else:
-        raise Exception('Compression type not supported')
+        raise Exception("Compression type not supported")
 
     # Declaration data class / named tuple
-    data_tuple = collections.namedtuple('Data', 'data metadata miniheader')
+    data_tuple = collections.namedtuple("Data", "data metadata miniheader")
 
     return data_tuple(data, header, miniheader)
 
@@ -220,7 +234,7 @@ def compress(data):
     :param data: Data to compress (numpy array)
     :return: Compressed data (numpy array - uint8-c-order)
     """
-    output_buffer = numpy.ndarray(data.nbytes, dtype=numpy.uint8, order='C')
+    output_buffer = numpy.ndarray(data.nbytes, dtype=numpy.uint8, order="C")
     compressed_size = cbf_c.compress(data, output_buffer)
 
     if compressed_size == 0:
@@ -238,7 +252,7 @@ def uncompress(binary_data, size_x, size_y, data_type):
 
     size = size_x * size_y * data_type(0).nbytes
 
-    output_buffer = b'a' * size
+    output_buffer = b"a" * size
     cbf_c.uncompress(binary_data, output_buffer)
     numpy_array = numpy.frombuffer(output_buffer, dtype=data_type)
     numpy_array = numpy_array.reshape(size_x, size_y)
--- setup.py
+++ setup.py
@@ -12,9 +12,15 @@ CBF_VERSION_STR = ".".join([str(x) for x in CBF_VERSION])
 
 compile_args = []
 if platform.system().lower() == "windows":
-    macros = [("VERSION", '\\"%s\\"' % VERSION_STR), ("CBF_VERSION", '\\"%s\\"' % CBF_VERSION_STR)]
+    macros = [
+        ("VERSION", '\\"%s\\"' % VERSION_STR),
+        ("CBF_VERSION", '\\"%s\\"' % CBF_VERSION_STR),
+    ]
 else:
-    macros = [("VERSION", '"%s"' % VERSION_STR), ("CBF_VERSION", '"%s"' % CBF_VERSION_STR)]
+    macros = [
+        ("VERSION", '"%s"' % VERSION_STR),
+        ("CBF_VERSION", '"%s"' % CBF_VERSION_STR),
+    ]
     compile_args.append("-O3")
 
 setup(
@@ -26,8 +32,17 @@ setup(
     url="https://github.com/paulscherrerinstitute/cbf",
     py_modules=["cbf"],
     ext_modules=[
-        Extension("cbf_c", ["src/cbf.cpp", "src/python-cbf.c"], define_macros=macros, extra_compile_args=compile_args)
+        Extension(
+            "cbf_c",
+            ["src/cbf.cpp", "src/python-cbf.c"],
+            define_macros=macros,
+            extra_compile_args=compile_args,
+        )
     ],
     install_requires=["numpy"],
-    classifiers=["Programming Language :: C", "Programming Language :: Python", "Programming Language :: Python :: 3"],
+    classifiers=[
+        "Programming Language :: C",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
 )
